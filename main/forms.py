1636c959ac2b09e92bdff1ede5d53bc994489448
Grey Baker <greysteil@gmail.com>
2011-12-12 09:54:14 +0000

    added error messages to edit profile page

--- main/forms.py
+++ main/forms.py
@@ -170,15 +170,15 @@ class ContactForm(forms.ModelForm):
     username = forms.EmailField(label=_("Email"), max_length=30,
         help_text = _("Required. 30 characters or fewer. Letters, digits and @/./+/-/_ only."),
         error_messages = {'invalid': _("Please enter a valid email address. It should be 30 characters or fewer."),
-                            'required': _("You didn't enter an email address. We need that!")},
+                            'required': _("You didn't enter an email address!")},
         widget = TextInput(attrs={'placeholder':'Email', 'label':'email'}))
     name = forms.CharField(label=_("Name"), max_length=30,
         help_text = _("This is the name you'll be known as on the site."),
-        error_messages = {'required': _("You didn't enter a display name. We need that!")},
+        error_messages = {'required': _("You didn't enter a display name!")},
         widget = TextInput(attrs={'placeholder':'Display name', 'label':'name'}))
     telephone = fields.UKPhoneNumberField(reject=(None, 'premium', 'service'),
         help_text = _("Required. We use this to help organise, and to chase down drums."),
-        error_messages = {'required': _("You didn't enter a phone number. We need that!")},
+        error_messages = {'required': _("You didn't enter a phone number!")},
         widget = TextInput(attrs={'placeholder':'Mobile phone number', 'label':'telephone'}))
     
     def __init__(self, *args, **kwargs):
