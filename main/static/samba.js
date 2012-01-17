057e7879b52182e18d5324575dbf86aa1b08b9ef
Grey Baker <greysteil@gmail.com>
2012-01-17 12:32:25 +0000

    fixed the lightsOut jQuery stuff

--- main/static/samba.js
+++ main/static/samba.js
@@ -13,40 +13,55 @@ $(function() {
 });
 
 // display a lights-out dialogue box
-$(function() {
+$(function lightsOut() {
     // Set up structure to put a message box in the centre of a dimmed screen
     $("body").prepend('<div id="dim"><div id="msgbox"></div></div>');
     $("#dim").css({ position:"absolute", top:0, left:0, width:"100%", height:$(document).height(), "z-index":20000,
                         background:'url("http://buildinternet.com/live/lightsout/dim.png")',
                         display:"none", "text-align":"left" });
-    $("#msgbox").css({ position:"fixed", width:940, "z-index":200,
+    $("#msgbox").css({ position:"fixed", width:940, "z-index":30000,
                         top:$(window).height()/2, left:"50%"});
     $("#msgbox").css({ "margin-left":-$("#msgbox").width()/2 });
+    // Take care of window resizing
+	$(window).bind("resize", function(){
+	 	$("#dim").css("height", $(document).height());
+	 	$("#msgbox").css("top", $(window).height()/2);
+	});
     
-    // When an alertdim link is clicked dim the screen and get the required form from the server
+    // When an alertdim link is clicked...
     $(".alertdim").click(function(){
         var self = this;
+        // load new content from the server into the message box...
         $("#msgbox").load($(this).attr("href") + " form:eq(0)", function() {
-            $("#msgbox").css({ "margin-top":-$("#msgbox").height()/2 });        // centre the alert box vertically
-            $("#msgbox .closedim").click(function() {                           // set up close links
+            $("#msgbox").css({ "margin-top":-$("#msgbox").height()/2 });
+            $("#msgbox .closedim").click(function() {
         		$("#dim").fadeOut(function() {$("#msgbox").empty();});
         		return false;
         	});
+        	// hijack the new form's submit method...
         	$("#msgbox").submit(function(){
-        	    var action = $("#msgbox form").attr("action") ? $("#msgbox form").attr("action") !== "" : $(self).attr("href");
+        	    // disable any more submits
+        	    $("#msgbox form [type=submit]").attr("disabled", "disabled")
+        	    // set the action path of the form
+        	    var action = $("#msgbox form").attr("action")
+        	    action = action ? action !== "" : $(self).attr("href")
+                // post form to the server
         	    $.ajax({
         	        type: 'POST',
         	        url: action,
         	        data: $("#msgbox form").serialize(),
         	        success: function(data) {
-        	            if ($(data).find("form .clearfix.error").length) {
+        	            if ($(data).find(".clearfix.error").length) {
                             $("#msgbox form").replaceWith($(data).find("form:eq(0)"));
-                            $("#msgbox .closedim").click(function() {                           // set up close links
+                            $("#msgbox .closedim").click(function() {
                         		$("#dim").fadeOut(function() {$("#msgbox").empty();});
                         		return false;
                         	});
                         } else {
-                            $("#dim").fadeOut(function() {location.reload();});
+                            $("#dim").fadeOut(function() {
+                                $("body").empty().prepend($(data).filter(function(i) {return $(this).is("div");}));
+                                $(lightsOut());
+                            });
                         }
                     },
         	        dataType: 'html'
@@ -56,11 +71,5 @@ $(function() {
         });
      	$("#dim").fadeIn();
 		return false;
-	});
-    
-    // Take care of window resizing
-	$(window).bind("resize", function(){
-	 	$("#dim").css("height", $(document).height());
-	 	$("#msgbox").css("top", $(window).height()/2);
 	});
 });
